87fc2c45fc7a1c100736db1e44d5ede38e534393
phuongzzz <yesterdayoncemore23@gmail.com>
2020-05-01 20:41:24 +0700

    lesson 2.7: custom transition 2

--- SwiftUI101/CourseList.swift
+++ SwiftUI101/CourseList.swift
@@ -26,36 +26,63 @@ struct CourseView: View {
     @State var show = false
     
     var body: some View {
-        VStack {
-            HStack {
-                VStack(alignment: .leading, spacing: 8.0) {
-                    Text("SwiftUI Advanced")
-                        .font(.system(size: 24, weight: .bold))
-                        .foregroundColor(.white)
-                    Text("20 sections")
-                        .foregroundColor(Color.white.opacity(0.7))
+        ZStack(alignment: .top) {
+            VStack(alignment: .leading, spacing: 30.0) {
+                Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen boo")
+                Text("About this course")
+                    .font(.title).bold()
+                Text("Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sde Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.")
+            }
+            .padding(30)
+            .frame(maxWidth: show ? .infinity : screen.width - 60,
+                maxHeight: show ? .infinity : 280)
+                .offset(y: show ? 460 : 0)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+                .opacity(show ? 1 : 0)
+
+            VStack {
+                HStack {
+                    VStack(alignment: .leading, spacing: 8.0) {
+                        Text("SwiftUI Advanced")
+                            .font(.system(size: 24, weight: .bold))
+                            .foregroundColor(.white)
+                        Text("20 sections")
+                            .foregroundColor(Color.white.opacity(0.7))
+                    }
+                    Spacer()
+                    ZStack {
+                        Image(uiImage: #imageLiteral(resourceName: "Logo1"))
+                            .opacity(show ? 0 : 1)
+                        Image(systemName: "xmark")
+                            .font(.system(size: 16, weight: .medium))
+                            .foregroundColor(.white)
+                            .frame(width: 36, height: 36)
+                            .background(Color.black)
+                            .clipShape(Circle())
+                            .opacity(show ? 1: 0)
+                    }
                 }
                 Spacer()
-                Image(uiImage: #imageLiteral(resourceName: "Logo1"))
+                Image(uiImage: #imageLiteral(resourceName: "Card2"))
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 140, alignment: .top)
             }
-            Spacer()
-            Image(uiImage: #imageLiteral(resourceName: "Card2"))
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(maxWidth: .infinity)
-                .frame(height: 140, alignment: .top)
+            .padding(show ? 30 : 20)
+            .padding(.top, show ? 30 : 0)
+            .frame(maxWidth: show ? .infinity : screen.width - 60,
+                    maxHeight: show ? 460 : 280)
+            .background(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)).opacity(0.3), radius: 20, x: 0, y: 20)
+            .onTapGesture {
+                self.show.toggle()
+            }
         }
-        .padding(show ? 30 : 20)
-        .padding(.top, show ? 30 : 0)
-        .frame(maxWidth: show ? .infinity : screen.width - 60,
-                maxHeight: show ? .infinity : 280)
-        .background(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
-        .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-        .shadow(color: Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)).opacity(0.3), radius: 20, x: 0, y: 20)
         .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-        .onTapGesture {
-            self.show.toggle()
-        }
         .edgesIgnoringSafeArea(.all)
     }
 }
