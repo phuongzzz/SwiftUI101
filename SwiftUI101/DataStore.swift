a50d3b0170e7efaddab75b264f96aba255373f58
phuongzzz <yesterdayoncemore23@gmail.com>
2020-05-02 15:36:11 +0700

    lesson 2.15: API and Combine

---
+++ SwiftUI101/DataStore.swift
@@ -0,0 +1,24 @@
+//
+//  DataStore.swift
+//  SwiftUI101
+//
+//  Created by phuongzzz on 5/2/20.
+//  Copyright © 2020 phuongzzz. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+class DataStore: ObservableObject {
+    @Published var posts: [Post] = []
+    
+    init() {
+        getPosts()
+    }
+    
+    func getPosts() {
+        Api().getPosts { posts in
+            self.posts = posts
+        }
+    }
+}
